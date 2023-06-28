81524f30b7132fe64c9cd6511da5b79f34bf6f29
Lucas Borali <129214234+LucasBorali@users.noreply.github.com>
2023-06-28 18:09:37 -0300

    Almost finished the projects section. All that is left is to add the projects description and inplement scroll buttons to the carousel

--- src/Assets/Constant.js
+++ src/Assets/Constant.js
@@ -2,21 +2,63 @@ import sneaker from './sneakers-product-page.png';
 import ageCalc from './age-calc.png';
 import notificationPage from './notifications-page.png';
 
+import html from './html-1.svg'
+import css from './css-3.svg'
+import js from './javascript-1.svg'
+import react from './react-2.svg'
+
+
 export const projects = [
   {
     img: sneaker,
+    repo: 'https://github.com/LucasBorali/sneakers-product-page',
+    website: 'https://lucasborali.github.io/sneakers-product-page/',
+    stack: [html, css, js, react],
     title: 'Sneakers Product Page',
     description:
       'loremaskndnoajfjojafnanf asdoashfjoasnoidi asnoifjjasjfioas ansoashiofa iaodiasjoia asnfiasjfasmldmaiij asnfoaijsd nasdioasj',
   },
   {
     img: ageCalc,
+    repo: 'https://github.com/LucasBorali/age-calculator-app-main',
+    website:'https://lucasborali.github.io/age-calculator-app-main/',
+    stack: [html, css, js],
     title: 'Age Calculator App',
     description:
       'loremaskndnoajfjojafnanf asdoashfjoasnoidi asnoifjjasjfioas ansoashiofa iaodiasjoia asnfiasjfasmldmaiij asnfoaijsd nasdioasj',
   },
   {
     img: notificationPage,
+    repo: 'https://github.com/LucasBorali/notifications-page-main',
+    website: 'https://lucasborali.github.io/notifications-page-main/',
+    stack: [html, css, js],
+    title: 'Notifications page',
+    description:
+      'loremaskndnoajfjojafnanf asdoashfjoasnoidi asnoifjjasjfioas ansoashiofa iaodiasjoia asnfiasjfasmldmaiij asnfoaijsd nasdioasj',
+  },
+  {
+    img: notificationPage,
+    repo: 'https://github.com/LucasBorali/notifications-page-main',
+    website: 'https://lucasborali.github.io/notifications-page-main/',
+    stack: [html, css, js],
+    title: 'Notifications page',
+    description:
+      'loremaskndnoajfjojafnanf asdoashfjoasnoidi asnoifjjasjfioas ansoashiofa iaodiasjoia asnfiasjfasmldmaiij asnfoaijsd nasdioasj',
+  },
+  {
+    img: notificationPage,
+    repo: 'https://github.com/LucasBorali/notifications-page-main',
+    website: 'https://lucasborali.github.io/notifications-page-main/',
+    stack: [html, css, js],
+    title: 'Notifications page',
+    description:
+      'loremaskndnoajfjojafnanf asdoashfjoasnoidi asnoifjjasjfioas ansoashiofa iaodiasjoia asnfiasjfasmldmaiij asnfoaijsd nasdioasj',
+  },
+  {
+    img: notificationPage,
+    repo: 'https://github.com/LucasBorali/notifications-page-main',
+    website: 'https://lucasborali.github.io/notifications-page-main/',
+    stack: [html, css, js],
     title: 'Notifications page',
     description:
       'loremaskndnoajfjojafnanf asdoashfjoasnoidi asnoifjjasjfioas ansoashiofa iaodiasjoia asnfiasjfasmldmaiij asnfoaijsd nasdioasj',
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import {React, useRef} from 'react'
 import logo from '../Assets/logo-alpha-channel-white.png'
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
+ 
+
+
   return (
     <nav className={classes.navBar}>
        
        <ul>
-        <li><a href='./'>Home</a></li>
+        <li ><a href='#home'>Home</a></li>
         <li><a href='#projects'>Projects</a></li>
         <li><a href='#about-me'>About Me</a></li>
         <li><a href='./'>Contact</a></li>
