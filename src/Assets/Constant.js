a3109dab743b41bf8287bcdd580fa172e8d10bd3
Lucas Borali <129214234+LucasBorali@users.noreply.github.com>
2023-06-29 18:23:16 -0300

    Almost done with the projects section. Just need to do an animation to the panel. The section 'About Me' is almost done too, with only my description and hard skills left finalize

--- src/Assets/Constant.js
+++ src/Assets/Constant.js
@@ -6,6 +6,14 @@ import html from './html-1.svg'
 import css from './css-3.svg'
 import js from './javascript-1.svg'
 import react from './react-2.svg'
+import communication from './communication.png'
+import problemSolving from './problem-solving.png'
+import attentionDetails from './attention-details.png'
+import timeMenagement from './time-menagement.png'
+import teamWork from './team-work.png'
+import adaptability from './adaptability.png'
+import creativity from './creativity.png'
+import criticalThinking from './critical-thinking.png'
 
 
 export const projects = [
@@ -36,31 +44,6 @@ export const projects = [
     description:
       'loremaskndnoajfjojafnanf asdoashfjoasnoidi asnoifjjasjfioas ansoashiofa iaodiasjoia asnfiasjfasmldmaiij asnfoaijsd nasdioasj',
   },
-  {
-    img: notificationPage,
-    repo: 'https://github.com/LucasBorali/notifications-page-main',
-    website: 'https://lucasborali.github.io/notifications-page-main/',
-    stack: [html, css, js],
-    title: 'Notifications page',
-    description:
-      'loremaskndnoajfjojafnanf asdoashfjoasnoidi asnoifjjasjfioas ansoashiofa iaodiasjoia asnfiasjfasmldmaiij asnfoaijsd nasdioasj',
-  },
-  {
-    img: notificationPage,
-    repo: 'https://github.com/LucasBorali/notifications-page-main',
-    website: 'https://lucasborali.github.io/notifications-page-main/',
-    stack: [html, css, js],
-    title: 'Notifications page',
-    description:
-      'loremaskndnoajfjojafnanf asdoashfjoasnoidi asnoifjjasjfioas ansoashiofa iaodiasjoia asnfiasjfasmldmaiij asnfoaijsd nasdioasj',
-  },
-  {
-    img: notificationPage,
-    repo: 'https://github.com/LucasBorali/notifications-page-main',
-    website: 'https://lucasborali.github.io/notifications-page-main/',
-    stack: [html, css, js],
-    title: 'Notifications page',
-    description:
-      'loremaskndnoajfjojafnanf asdoashfjoasnoidi asnoifjjasjfioas ansoashiofa iaodiasjoia asnfiasjfasmldmaiij asnfoaijsd nasdioasj',
-  },
 ];
+
+export const icons = [communication, problemSolving, attentionDetails, timeMenagement, teamWork, adaptability , creativity, criticalThinking]
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,14 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './AboutMe.module.css'
+import pictureDesktop from '../Assets/picture-desktop.jpeg'
+import {icons} from '../Assets/Constant'
+
 
 const AboutMe = () => {
+
+
+
+  const [selectedButton, setSelectedButton] = useState('Meet Me')
+
+
+const selectButtonHandler = function(e) {
+  if(e.target.id === 'meet-me') {
+    setSelectedButton('Meet Me')
+  }
+  if(e.target.id === 'hard-skills') {
+    setSelectedButton('Hard Skills')
+  }
+  if(e.target.id === 'soft-skills' ) {
+    setSelectedButton('Soft Skills')
+  }
+}
+
   return (
     <section id='about-me' className={classes.aboutMe}>
       <div className={classes.head}>
         <p className={classes.back}>ABOUT ME</p>
         <p className={classes.front}>Who I am</p>
       </div>
+      <div className={classes.panel}>
+        <img src={pictureDesktop} alt={pictureDesktop}/>
+        <div>
+          <ul onClick={selectButtonHandler} className={classes.buttons}>
+            <li><button className={selectedButton === 'Meet Me' ? classes.selected : classes.unselected } id='meet-me'>Meet Me</button></li>
+            <li><button className={selectedButton === 'Hard Skills' ? classes.selected : classes.unselected } id='hard-skills'>Hard Skills</button></li>
+            <li><button className={selectedButton === 'Soft Skills' ? classes.selected : classes.unselected } id='soft-skills'>Soft Skills</button></li>
+          </ul>
+          {selectedButton === 'Meet Me' ?<div className={classes['display-panel']}><p>other thing</p></div>: ''}
+          {selectedButton === 'Hard Skills' ? <div className={classes['display-panel']}><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate sapiente dolor necessitatibus nobis nulla voluptatum optio neque assumenda cupiditate, similique culpa nihil dolores placeat vitae fuga facilis, ipsa natus quae!</p></div>: ''}
+          {selectedButton === 'Soft Skills' ? <div className={classes['display-panel']}>
+            <ul className={classes['soft-skills-panel']}>
+              <li>Communication <img src={icons[0]} alt={icons[0]}/> </li>
+              <li>Problem Solving<img src={icons[1]} alt={icons[1]}/></li>
+              <li>Attention to Detail  <img src={icons[2]} alt={icons[2]}/></li>
+              <li>Time Menagement  <img src={icons[3]} alt={icons[3]}/></li>
+              <li>Team Work  <img src={icons[4]} alt={icons[4]}/></li>
+              <li>Adaptability  <img src={icons[5]} alt={icons[5]}/></li>
+              <li>Creativity  <img src={icons[6]} alt={icons[6]}/></li>
+              <li>Critical Thinking  <img src={icons[7]} alt={icons[7]}/></li>
+            </ul>
+          </div>: ''}
+        </div>
+      </div>
     </section>
+
   )
 }
 
