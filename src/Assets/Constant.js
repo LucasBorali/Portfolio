dcb407ba0fb4fa96a1ee62c1ea1542d6ba49e580
Lucas Borali <129214234+LucasBorali@users.noreply.github.com>
2023-08-26 11:23:04 -0300

    fixed the projects list design and added description for car rental app

--- src/Assets/Constant.js
+++ src/Assets/Constant.js
@@ -1,8 +1,7 @@
 import sneaker from './sneakers-product-page.png';
-import ageCalc from './age-calc.png';
 import notificationPage from './notifications-page.png';
-import caraECommerce from './cara-e-commerce.png'
-import carRental from './car-rental-app.png'
+import caraECommerce from './cara-e-commerce.png';
+import carRental from './car-rental-app.png';
 
 import html from './html-1.svg';
 import css from './css-3.svg';
@@ -18,17 +17,16 @@ import teamWork from './team-work.png';
 import adaptability from './adaptability.png';
 import creativity from './creativity.png';
 import criticalThinking from './critical-thinking.png';
-import firebase from './firebase-2.svg'
+import firebase from './firebase-2.svg';
 
 export const projects = [
   {
     img: carRental,
-    repo: 'https://github.com/LucasBorali/cara-e-commerce',
-    website: 'https://lucasborali.github.io/cara-e-commerce/',
+    repo: 'https://github.com/LucasBorali/car-rental',
+    website: 'https://car-rental-ec71e.web.app/',
     stack: [html, css, js, react, firebase],
-    title: 'Cara e-commerce',
-    description:
-      `Immerse yourself in a sleek and captivating design that seamlessly blends form and function. Explore a diverse range of products with ease, accompanied by an intuitive navigation system. Enjoy a visually appealing and user-friendly interface that elevates your online shopping journey to new heights.`,
+    title: 'Car rental',
+    description: `A user-friendly and fully responsive car rental platform that offers a seamless experience for users looking to rent vehicles; Additionally, the platform offers a direct means of communication by allowing users to easily get in touch with the company's team if they require assistance or have inquiries.`,
   },
   {
     img: caraECommerce,
@@ -36,8 +34,7 @@ export const projects = [
     website: 'https://lucasborali.github.io/cara-e-commerce/',
     stack: [html, css, js, react, firebase],
     title: 'Cara e-commerce',
-    description:
-      `Immerse yourself in a sleek and captivating design that seamlessly blends form and function. Explore a diverse range of products with ease, accompanied by an intuitive navigation system. Enjoy a visually appealing and user-friendly interface that elevates your online shopping journey to new heights.`,
+    description: `Immerse yourself in a sleek and captivating design that seamlessly blends form and function. Explore a diverse range of products with ease, accompanied by an intuitive navigation system. Enjoy a visually appealing and user-friendly interface that elevates your online shopping journey to new heights.`,
   },
   {
     img: sneaker,
@@ -45,8 +42,7 @@ export const projects = [
     website: 'https://lucasborali.github.io/sneakers-product-page/',
     stack: [html, css, js, react],
     title: 'Sneakers Product Page',
-    description:
-      `As you're about to see, 'Sneakers product page' is, for now, pretty much what the name says: a product page. At first, the prompt was to just recreate the design with no fancy interactions, but I have built this product page to be a component that can be easily connected to an API or any other kind of data bank.`,
+    description: `As you're about to see, 'Sneakers product page' is, for now, pretty much what the name says: a product page. At first, the prompt was to just recreate the design with no fancy interactions, but I have built this product page to be a component that can be easily connected to an API or any other kind of data bank.`,
   },
   {
     img: notificationPage,
@@ -55,7 +51,7 @@ export const projects = [
     stack: [html, css, js],
     title: 'Notifications page',
     description:
-      "A notification component that can be easily integrated on any kind of social app.",
+      'A notification component that can be easily integrated on any kind of social app.',
   },
 ];
 
